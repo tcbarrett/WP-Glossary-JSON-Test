1bf86667c52d4088331455bff086fadd0965ca48
Ryan McCue <me+github@ryanmccue.info>
2013-12-11 20:44:18 +1000

    Pass the server into the endpoint

--- wp-glossary-json-test.php
+++ wp-glossary-json-test.php
@@ -7,10 +7,10 @@
  * Author URI: http://www.tcbarrett.com/
  */
 
-add_action( 'wp_json_server_before_serve', 'tcb_glossary_json_api_init' );
-function tcb_glossary_json_api_init() {
+add_action( 'wp_json_server_before_serve', 'tcb_glossary_json_api_init', 10, 1 );
+function tcb_glossary_json_api_init( $server ) {
 	global $glossary_json_api;
 
 	require_once dirname( __FILE__ ) . '/class-json-api-glossary.php';
-	$glossary_json_api = new Glossary_JSON_API();
+	$glossary_json_api = new Glossary_JSON_API( $server );
 }
